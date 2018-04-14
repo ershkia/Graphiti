ece07e55f7a2c33308552333dca5618723642d3c
ershad <ershkia@gmaio.com>
2018-04-14 10:14:31 -0400

    Initial commit

---
+++ Graphiti.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Graphiti.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
